8c324d7d9342542d2568da8370b3b96804e7fb0c
Rácz Ádám <racz.adam.istvan@gmail.com>
2023-05-20 19:45:50 +0200

    edit Driver.ts, add Rectangle.ts

--- 05-inheritance/Driver.ts
+++ 05-inheritance/Driver.ts
@@ -1,5 +1,13 @@
 import {Shape} from './Shape';
+import {Circle} from './Circle';
+import { Rectangle } from './Rectangle';
 
+let myShape = new Shape(10, 15);
+let myCircle = new Circle(5, 10, 20);
 
+console.log(myShape.getInfo());
+console.log(myCircle.getInfo());
 
-let myShape = new Shape(1, 2);
+let myRectangle = new Rectangle(0, 0, 3, 5);
+
+console.log(myRectangle.getInfo());
