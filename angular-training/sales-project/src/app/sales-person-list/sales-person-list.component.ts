e06e534760501e4843a95b1150e7f989e0008804
Rácz Ádám <racz.adam.istvan@gmail.com>
2023-05-21 19:50:19 +0200

    missed from the last commit

---
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList:  SalesPerson[] = [
+    new SalesPerson("Jeno", "Kovacs", "jeno@kovacs.org", 30000),
+    new SalesPerson("Red", "Mother", "redmother@rm.org", 40000),
+    new SalesPerson("John", "Doe", "jon@doe.org", 60000),
+    new SalesPerson("Test", "Esther", "test@test.com", 50000)
+  ];
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+    console.log("ngOnInit -> invoked");
+  }
+
+}
